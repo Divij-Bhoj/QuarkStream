9e4705ca407b39cdc2a552d1717f96165396f5af
Divij Bhoj <divijbhoj@gmail.com>
2026-03-01 17:58:10 +0000

    Fix AiInferenceEngine test compilation error

--- src/test/java/com/example/demo/AiInferenceEngineTests.java
+++ src/test/java/com/example/demo/AiInferenceEngineTests.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AiInferenceEngineTests {
 
-    private final AiInferenceEngine inferenceEngine = new AiInferenceEngine();
+    @Autowired
+    private AiInferenceEngine inferenceEngine;
+
 
     @Test
     void testAnomalyDetectionIsProbabilistic() {
